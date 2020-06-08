faeadd210822daff462bacc9c5877dcfa714eb54
Mikasb <beliackasmikas@gmail.com>
2020-06-09 01:54:12 +0300

    test commit

--- src/appUtils/weather.js
+++ src/appUtils/weather.js
@@ -1,11 +1,10 @@
 /**
- * 
- * @param {*} longitude - longitude of the given city (e.g. 52.66)
- * @param {*} latitude - latitude of the given city (e.g. 22.3)
- * @param {*} startTime - observation start date (e.g. 2020-06-08T09:20:00.000Z)
- * @param {*} endTime - observation end date (e.g. 2020-06-07T09:20:00.000Z)
- * @param {*} units - si/us to retrieve data in celsius or fahrenheit
- * @return - URL to access the API json data
+ * @param {*} longitude - longitude of the given city (e.g. 52.66).
+ * @param {*} latitude - latitude of the given city (e.g. 22.3).
+ * @param {*} startTime - observation start date (e.g. 2020-06-08T09:20:00.000Z).
+ * @param {*} endTime - observation end date (e.g. 2020-06-07T09:20:00.000Z).
+ * @param {*} units - si/us to retrieve data in celsius or fahrenheit.
+ * @return - URL to access the API json data.
  */
 const weather = (longitude, latitude, startTime, endTime, units) => {
     const apiKey = 'g7pYjMY0G8IuD3tJUWnF1KqzOa6ac3w3';
